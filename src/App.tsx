31f1e71f36755aaf2a4b49090d8dfaa1a33f3333
Sara Lavanchy <saralavanchy94@gmail.com>
2023-05-30 13:26:24 -0400

    add pii

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { saveUser } from "user-service";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    address: "",
+    telephone: "",
+    payment: {
+      creditCardNumber: "",
+      vcd: "",
+      expirationDate: "",
+      cardName: ""
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +31,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <form onSubmit={()=> saveUser(user)}>
+        <p>Your data:</p>
+        <input type="text" placeholder="name" onChange={e => setUser({...user, name: e.target.value })}  />
+        <input type="text" placeholder="address" onChange={e => setUser({...user, address: e.target.value })}  />
+        <input type="tel" placeholder="telephone" onChange={e => setUser({...user, telephone: e.target.value })}  />
+        <p>Payment: </p>
+
+        <button type="submit">Save info</button>
+      </form>
     </div>
   );
 }
